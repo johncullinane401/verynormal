f646f94e85639e0209fc3b47e5ebca8d009c7cce
johncullinane401 <jpaul.cull@gmail.com>
2026-07-27 16:48:20 -0400

    added start of portal 1

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -59,7 +59,7 @@
 #define FLAG_LEARNED_DD    0x25 // Unused Flag
 #define FLAG_FIRST_YAN_MEETING    0x26 // Unused Flag
 #define FLAG_YIN_SPIEL    0x27 // Unused Flag
-#define FLAG_UNUSED_0x028    0x28 // Unused Flag
+#define FLAG_PORTAL_ACCESS    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
 #define FLAG_UNUSED_0x02A    0x2A // Unused Flag
 #define FLAG_UNUSED_0x02B    0x2B // Unused Flag

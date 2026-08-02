afbd84fdc7c1a8cfdfe4552b67272482dd4120b9
johncullinane401 <jpaul.cull@gmail.com>
2026-08-02 16:31:04 -0400

    test:

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1750,3 +1750,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/GREYFOREST1/scripts.inc"
 
 	.include "data/maps/FORESTOFREFLECTION/scripts.inc"
+
+	.include "data/maps/WEIRDCAVEBRIDGE/scripts.inc"
